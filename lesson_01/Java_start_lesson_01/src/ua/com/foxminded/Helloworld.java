6fa05bfebd15dc8156988bfae05db194439ade73
Ars Hevorkian <gevoars@gmail.com>
2026-06-17 15:20:09 +0200

    lesson 01-3

--- lesson_01/Java_start_lesson_01/src/ua/com/foxminded/Helloworld.java
+++ lesson_01/Java_start_lesson_01/src/ua/com/foxminded/Helloworld.java
@@ -8,6 +8,9 @@ public class Helloworld {
 String message_en = "Hello world";
 String message_de = "Hallo Welt";
 System.out.println(message_de);
+int zahl = 1654;
+String buchstabe = "N UA";
+System.out.println(buchstabe + " " + zahl);
 	}
 
 }
